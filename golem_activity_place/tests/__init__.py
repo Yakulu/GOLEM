58f71503e5f1d39ffd93233d7784eb87ab93178a
Fabien Bourgeois <fabien@yaltik.com>
2017-06-19 18:14:18 +0200

    [QUA]Fix tests for activity places (bad import)

--- golem_activity_place/tests/__init__.py
+++ golem_activity_place/tests/__init__.py
@@ -15,4 +15,4 @@
 #    You should have received a copy of the GNU Affero General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from . import test_golem_activity_session
+from . import test_golem_activity
