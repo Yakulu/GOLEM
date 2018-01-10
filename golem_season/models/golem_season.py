485a45f217c5add6985d8a4472dc8fe61e00578b
michel <michel.dessenne@gmail.com>
2018-01-10 21:51:39 +0100

    [TPM] Membership type -> Link season

--- golem_season/models/golem_season.py
+++ golem_season/models/golem_season.py
@@ -29,7 +29,10 @@ class GolemSeason(models.Model):
 
     name = fields.Char('Season name', copy=False)
 
-    membership_id = fields.Many2one('product.product', 'Membership type')
+    membership_id = fields.Many2one('product.template', 'Membership type',
+                                    domain="[('membership', '=', True)]" )
+
+
     member_counter = fields.Integer('Counter for member number generation',
                                     readonly=True, default=1)
     date_start = fields.Date('Period start')
