a0cc9dcb489adebde823191c2f22ffc2cf927762
michel <michel.dessenne@gmail.com>
2018-01-15 15:40:22 +0100

    [TMP] Test

--- golem_season/tests/test_golem_season.py
+++ golem_season/tests/test_golem_season.py
@@ -58,9 +58,9 @@ class TestGolemSeason(TransactionCase):
     def test__onchange_season_dates(self):
         """ Test if membership date """
         # je cree un article adhésion
-        membership_id = self.env['product.template'].create({'display_name': 'Name',
+        membership_id = self.env['product.template'].create({'name': 'Name',
                                                              'membership_date_from': '2009-11-01',
-                                                             'membership_date_to': '2009-11-01'})
+                                                             'membership_date_to': '2010-11-01'})
 
 # je cree une saison attachée a l'article adhésion
         new_season = self.env['golem.season'].create({'name': 'Name',
