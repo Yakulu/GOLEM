1307a5458ba9d12f102eefc299acf4df8863d409
michel <michel.dessenne@gmail.com>
2018-02-01 17:27:40 +0100

    [TMP]Update number

--- golem_activity/__manifest__.py
+++ golem_activity/__manifest__.py
@@ -18,7 +18,7 @@
 {
     'name': 'GOLEM activities',
     'summary': 'Extends Odoo products for multi-activity',
-    'version': '10.0.2.0.1',
+    'version': '10.0.2.1.0',
     'category': 'GOLEM',
     'author': 'Fabien Bourgeois, Michel Dessenne',
     'license': 'AGPL-3',
