20c57c5e571ebf2d38d1a2304f9564fb3fd70a83
eloyoussef <elo.yusuf@gmail.com>
2018-02-19 23:58:50 +0100

    organise files and configure the activity queue register button

--- golem_activity_queue/models/golem_activity_queue.py
+++ golem_activity_queue/models/golem_activity_queue.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
-#    Copyright 2017 Fabien Bourgeois <fabien@yaltik.com>
+#    Copyright 2018 Youssef El Ouahby <youssef@yaltik.com>
+#    Copyright 2018 Fabien Bourgeois <fabien@yaltik.com>
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
@@ -41,65 +42,6 @@ class YesNoQueuePopUp(models.TransientModel):
     @api.multi
     def queueNo(self):
         pass # don't do anything
-
-
-class GolemActivity(models.Model):
-    """ GOLEM Activity adaptations """
-    _inherit = 'golem.activity'
-
-
-    #ajout d'un champs O2M vers activity_id
-    activity_queue_id = fields.One2many('golem.activity.queue', 'activity_id')
-    # un boolen pour determiner si une fille d'attente est autorisé
-    queue_allowed = fields.Boolean(default=True)
-
-    def queue_register(self):
-        return {
-            'name'      : _('Register in the queue'),
-            'type'      : 'ir.actions.act_window',
-            'res_model' : 'golem.activity.queue',
-            'view_mode': 'form',
-            'view_type': 'form',
-            'target': 'new',
-        }
-
-    @api.onchange('activity_registration_ids')
-    def _checkRemain(self):
-        
-        if len(self.activity_registration_ids) > self.places and self.queue_allowed:
-            return {
-                'warning' : {
-                    'title' : _('Warning'),
-                    'message': _('No remaining place, please register in the queue'),
-                }
-            }
-
-    """
-    @api.multi
-    @api.constrains('places_remain')
-    def _check_remaining_places(self):
-        #Forbid inscription when there is no more place
-        for activity in self:
-            if activity.places_remain < 5:
-
-                if self.queue_allowed:
-                    print "__________________________ test ______________________"
-                    return {
-                        'name'      : _('Do you want to add your registration to the queue?'),
-                        'type'      : 'ir.actions.act_window',
-                        'res_model' : 'golem.activity.queue',
-                        'view_mode': 'form',
-                        'view_type': 'form',
-                        'target': 'new',
-                        }
-                    print "________________________________test 2 __________________"
-                    raise models.ValidationError("erreur")
-
-
-                else:
-                    emsg = _('Sorry, there is no more place man !')
-                    raise models.ValidationError(emsg)
-"""
 
 
 
--- golem_activity_queue/models/golem_member.py
+++ golem_activity_queue/models/golem_member.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
-#    Copyright 2016 Fabien Bourgeois <fabien@yaltik.com>
+#    Copyright 2018 Youssef El Ouahby <youssef@yaltik.com>
+#    Copyright 2018 Fabien Bourgeois <fabien@yaltik.com>
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
@@ -14,7 +15,3 @@
 #
 #    You should have received a copy of the GNU Affero General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-from . import golem_activity_queue
-from . import golem_activity
-from . import golem_member
