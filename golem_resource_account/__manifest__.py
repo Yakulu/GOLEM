1d19c17f4a74ec08dcb8ed23fcec9709b007cb98
Fabien BOURGEOIS <fabien@yaltik.com>
2018-03-26 19:05:04 +0200

    [REL]Bump versions for GOLEM Resources modules

--- golem_resource_account/__manifest__.py
+++ golem_resource_account/__manifest__.py
@@ -20,7 +20,7 @@
     'name': 'GOLEM resources invoicing',
     'summary': 'GOLEM resources invoicing',
     'description': ''' GOLEM resources invoicing ''',
-    'version': '10.0.0.0.2',
+    'version': '10.0.0.1.1',
     'category': 'GOLEM',
     'author': 'Youssef El Ouahby, Fabien Bourgeois',
     'license': 'AGPL-3',
