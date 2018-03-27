671cb68b86981d03f389fbfacfcb73aaf8837c52
Fabien BOURGEOIS <fabien@yaltik.com>
2018-03-27 15:58:54 +0200

    [IMP]GOLEM Member : new tools menu

--- golem_member/__manifest__.py
+++ golem_member/__manifest__.py
@@ -19,7 +19,7 @@
 {
     'name': 'GOLEM non-profit members',
     'summary': 'Extends Odoo contacts for MJC',
-    'version': '10.0.1.1.4',
+    'version': '10.0.1.1.5',
     'category': 'GOLEM',
     'author': 'Fabien Bourgeois, Michel Dessenne',
     'license': 'AGPL-3',
