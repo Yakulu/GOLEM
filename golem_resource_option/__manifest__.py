f710947771cc5306ee122e4cb746b39a90b4d273
Fabien BOURGEOIS <fabien@yaltik.com>
2018-03-29 10:02:41 +0200

    [FIX]GOLEM Resource Option : should not see other options on own definition list on ressource form

--- golem_resource_option/__manifest__.py
+++ golem_resource_option/__manifest__.py
@@ -22,7 +22,7 @@
     'description': ''' GOLEM resources options management :
     - create 1:n options per resource ;
     - choose option on reservations ''',
-    'version': '10.0.0.1.0',
+    'version': '10.0.0.1.1',
     'category': 'GOLEM',
     'author': 'Youssef El Ouahby, Fabien Bourgeois',
     'license': 'AGPL-3',
