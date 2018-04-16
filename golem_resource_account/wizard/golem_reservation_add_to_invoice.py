4104fbace983486a0b48a95e38cb151a07054743
Fabien BOURGEOIS <fabien@yaltik.com>
2018-04-16 18:14:02 +0200

    [REF][REM]GOLEM Resource Account : remove useless methods on wizard for adding on invoice

--- golem_resource_account/wizard/golem_reservation_add_to_invoice.py
+++ golem_resource_account/wizard/golem_reservation_add_to_invoice.py
@@ -17,7 +17,7 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """ GOLEM Reservation's Adding to invoice wizard"""
 
-from odoo import models, fields, api, _
+from odoo import models, fields
 
 class GolemReservationAddToInvoiceWizard(models.TransientModel):
     """GOLEM Reservation Add to Invoice Wizard """
@@ -25,30 +25,3 @@ class GolemReservationAddToInvoiceWizard(models.TransientModel):
 
     invoice_ids = fields.Many2many('account.invoice', string="Partner invoice list")
     reservation_id = fields.Many2one('golem.resource.reservation')
-
-    keyword = fields.Char(required=True)
-    member_ids = fields.Many2many('golem.member', string='Members')
-    contact_ids = fields.Many2many('res.partner', string='Contacts')
-
-    @api.multi
-    def action(self):
-        """ Returns action window with current model and instance """
-        self.ensure_one()
-        return {'name' : _('Search results'),
-                'type' : 'ir.actions.act_window',
-                'res_model' : self._name,
-                'res_id': self[0].id,
-                'view_mode': 'form',
-                'target': 'new'}
-
-    @api.multi
-    def search_partners(self):
-        """ Searches partners in name, email """
-        self.ensure_one()
-        domain = ['|',
-                  ('name', 'ilike', self[0].keyword),
-                  ('email', 'ilike', self[0].keyword)]
-        partner_ids = self.env['res.partner'].search(domain)
-        self[0].write({'contact_ids': [(6, False, partner_ids.ids)],
-                       'member_ids': [(6, False, partner_ids.mapped('member_id').ids)]})
-        return self[0].action()
