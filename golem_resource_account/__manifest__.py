1a66cbd81c3f437c3844d604bd875c8ad741e785
Fabien BOURGEOIS <fabien@yaltik.com>
2018-05-25 17:41:27 +0200

    [IMP]GOLEM Resource Account : better coherence for quantities and UoM, using XMLIDS

--- golem_resource_account/__manifest__.py
+++ golem_resource_account/__manifest__.py
@@ -20,7 +20,7 @@
     'name': 'GOLEM resources invoicing',
     'summary': 'GOLEM resources invoicing',
     'description': ''' GOLEM resources invoicing ''',
-    'version': '10.0.0.2.0',
+    'version': '10.0.0.3.0',
     'category': 'GOLEM',
     'author': 'Youssef El Ouahby, Fabien Bourgeois',
     'license': 'AGPL-3',
