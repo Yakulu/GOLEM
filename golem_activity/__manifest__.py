7439bea5594fc8bb52d706fe660c7150a1b56693
Fabien BOURGEOIS <fabien@yaltik.com>
2018-06-11 18:11:28 +0200

    [REL]GOLEM Activity : bump versions

--- golem_activity/__manifest__.py
+++ golem_activity/__manifest__.py
@@ -18,7 +18,7 @@
 {
     'name': 'GOLEM activities',
     'summary': 'Extends Odoo products for multi-activity',
-    'version': '10.0.2.1.3',
+    'version': '10.0.2.2.0',
     'category': 'GOLEM',
     'author': 'Fabien Bourgeois, Michel Dessenne',
     'license': 'AGPL-3',
