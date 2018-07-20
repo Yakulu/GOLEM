3270ceab13e14eefe3cc751dc2861df6db46d24a
Fabien BOURGEOIS <fabien@yaltik.com>
2018-07-20 18:24:39 +0200

    [I18N]Typo on translations

--- golem_activity_registration/__manifest__.py
+++ golem_activity_registration/__manifest__.py
@@ -18,7 +18,7 @@
 {
     'name': 'GOLEM Activity Member Registrations',
     'summary': 'GOLEM Activities Member Registrations management',
-    'version': '10.0.1.3.1',
+    'version': '10.0.1.3.3',
     'category': 'GOLEM',
     'author': 'Fabien Bourgeois, Michel Dessenne',
     'license': 'AGPL-3',
