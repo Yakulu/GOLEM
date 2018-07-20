5746a30a623af8357a006405b7abea364935f5b1
Fabien BOURGEOIS <fabien@yaltik.com>
2018-07-20 16:23:56 +0200

    [IMP]GOLEM Member(ship) : whole menu reorganization

--- golem_membership/__manifest__.py
+++ golem_membership/__manifest__.py
@@ -18,7 +18,7 @@
 {
     'name': 'GOLEM non-profit membership handling',
     'summary': 'Extends Odoo membership',
-    'version': '10.0.2.0.0',
+    'version': '10.0.2.1.0',
     'category': 'GOLEM',
     'author': 'Fabien Bourgeois, Michel Dessenne',
     'license': 'AGPL-3',
@@ -26,5 +26,6 @@
     'installable': True,
     'depends': ['account', 'decimal_precision'],
     'data': ['data/account_data.xml',
+             'views/membership_views.xml',
              'views/golem_membership_invoice.xml']
 }
