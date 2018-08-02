037a69d09e1ad4a7524a8bac720dd7fa6864712a
Fabien BOURGEOIS <fabien@yaltik.com>
2018-08-02 18:48:19 +0200

    [IMP]GOLEM Activity Registration : no more empty table for activities, invoices payments.

--- golem_activity_registration_payment/__manifest__.py
+++ golem_activity_registration_payment/__manifest__.py
@@ -19,7 +19,7 @@
     'name': 'GOLEM Activity Member Registration Payments',
     'summary': 'GOLEM Activities Member Registration Payments',
     'description': 'GOLEM Activities Member Registration Payments',
-    'version': '10.0.0.3.0',
+    'version': '10.0.0.3.1',
     'category': 'GOLEM',
     'author': 'Fabien Bourgeois',
     'license': 'AGPL-3',
