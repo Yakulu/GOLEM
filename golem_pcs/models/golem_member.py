fb626e4885e68bb02582cc22805e64fa357ac2ed
youssef <youssef@yaltik.com>
2018-09-17 13:47:57 +0100

    Add pcs_id to golem.member

---
+++ golem_pcs/models/golem_member.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+#    Copyright 2018 Youssef El Ouahby <youssef@yaltik.com>
+#    Copyright 2018 Fabien Bourgeois <fabien@yaltik.com>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+""" GOLEM Member adaptations """
+
+from odoo import models, fields, api, _
+
+class GolemMember(models.Model):
+    """ GOLEM Member extention """
+    _inherit = 'golem.member'
+
+    pcs_id = fields.Many2one('golem.pcs', string='Professions and Socioprofessional Categories',
+                             domain="[('parent_id', '!=', False)]")
