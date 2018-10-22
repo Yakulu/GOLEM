e5285ecebc115c6033ec9975f08e12a1d3c3a744
youssef <youssef@yaltik.com>
2018-10-22 20:57:54 +0100

    Edit init

--- golem_family_history/models/__init__.py
+++ golem_family_history/models/__init__.py
@@ -16,4 +16,4 @@
 #    You should have received a copy of the GNU Affero General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-#from . import golem_family
+from . import golem_family_history, golem_family
